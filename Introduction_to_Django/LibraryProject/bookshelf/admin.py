dabdee4518b5b3e86620259a2091638ed2d414f2
Gacheru <johngacheru533@gmail.com>
2025-07-14 14:20:56 +0300

    filter

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,7 +4,8 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_date')
+    list_filter =('publication_year')
     search_field = ('title', 'author')
-    
+
 admin.site.register(Book)
 
