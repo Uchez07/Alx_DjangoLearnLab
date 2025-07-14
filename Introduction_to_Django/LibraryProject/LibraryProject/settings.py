fda76fc63864b21125efe609ea67f20ee6d82594
Gacheru <johngacheru533@gmail.com>
2025-07-14 12:18:44 +0300

    modify app

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf.Apps.BookshelfConfig'
+    'bookshelf.apps.BookshelfConfig',
 ]
 
 MIDDLEWARE = [
