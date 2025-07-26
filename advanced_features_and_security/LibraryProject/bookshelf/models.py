cf85ecb8e15530fb39fb161c7794a0a4011d978c
Gacheru <johngacheru533@gmail.com>
2025-07-26 11:29:24 +0300

    user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -8,3 +8,8 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.CharField(max_length=100)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
