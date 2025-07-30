d0505e0bc95a0d8f66bd3bd4edea2582d378429c
Gacheru <johngacheru533@gmail.com>
2025-07-30 13:19:15 +0300

    indent

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,12 +7,12 @@ class Book(models.Model):
     publication_year = models.IntegerField()
 
         class Meta:
-        permissions = [
-            ("can_view_book", "Can view book"),
-            ("can_create_book", "Can create book"),
-            ("can_edit_book", "Can edit book"),
-            ("can_delete_book", "Can delete book"),
-        ]
+            permissions = [
+                ("can_view_book", "Can view book"),
+                ("can_create_book", "Can create book"),
+                ("can_edit_book", "Can edit book"),
+                ("can_delete_book", "Can delete book"),
+            ]
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
