109920744174deae9bb4b1a3b33a359def50c998
Gacheru <johngacheru533@gmail.com>
2025-08-01 16:31:01 +0300

    bn

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from django.db import models 
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
