dbed141310abf9bb44d0c5dd51ce20e515acd9d3
Gacheru <johngacheru533@gmail.com>
2025-08-01 16:29:47 +0300

    xcvb

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 class BookList(generics.ListAPIView):
