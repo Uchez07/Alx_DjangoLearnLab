05b30dea82c356fabbcb12d7082db2ddef9fc7f5
Gacheru <johngacheru533@gmail.com>
2025-08-06 11:58:14 +0300

    api urls

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
     path('get-token/', obtain_auth_token, name='get-token'),
 ]
