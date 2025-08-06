e6bd0bbd59ab2fe0a0f2180237cdbbab7d6ba0b6
Gacheru <johngacheru533@gmail.com>
2025-08-06 13:11:18 +0300

    asdf

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_year = models.DateField(max_length=100)
+    author = models.ForeignKe(Author, ondelete=models.CASCADE related_name='book')
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,23 @@
+from.models import Book, Author  # Ensure models are correctly imported
+from restframework import serializers
+from.serializers import BookSerializer  #assuming serializers is in the same field
+from.datetime import date
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = book
+        fields = '__all__'  #Return all the fields
+
+    def validate_date(self, values):  #This method is automatically calls the serializer validation for the publication_year field
+        current_year = date.today().year
+        if value >current_year:
+            raise serializer.ValidationError("The year does notexist")
+        return value
+
+
+class AuthorSerializer(serializer.ModelSerializer):
+    book = BookSerializer(many=True, read_only=True, source='book_set') #Nested books
+
+    class Meta: 
+        model = Author
+        fields = ['name'] #Returns only the name field
