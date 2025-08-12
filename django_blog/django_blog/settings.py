eb0adbdb8bf68df98268895fb4db56989ee348a4
Gacheru <johngacheru533@gmail.com>
2025-08-12 16:04:42 +0300

    Change the databaseto Postgress SQL and added Port and user

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'Gacheru',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',  # or your DB server
+        'PORT': '3306',       # default for PostgreSQL
     }
 }
 
