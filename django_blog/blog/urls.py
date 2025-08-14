2aa0428daab56b300224284f2d8f3eec6935ba23
Gacheru <johngacheru533@gmail.com>
2025-08-14 12:36:14 +0300

    asdfgh

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,6 +15,6 @@ urlpatterns =[
     path('posts/', ListView.as_view(), name='posts'),
     path('post/<int:pk>/', DetailView.as_view(), name='post-detail'),
     path('post/new/', CreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', UpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', UpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', DeleteView.as_view(), name='post-delete'),
 ]
