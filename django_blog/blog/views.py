a2be4fe7656e6a074bdc38d9bc43d56173ef83fe
Gacheru <johngacheru533@gmail.com>
2025-08-18 16:49:53 +0300

    added ca commentcreateview for the model

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -122,6 +122,21 @@ def add_comment(request, pk):
     else:
         form = CommentForm()
     return redirect('post-detail', pk=post.pk)  # fallback
+#Create new comment
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def form_valid(self, form):
+        # Attach the logged-in user and post
+        form.instance.author = self.request.user
+        post = get_object_or_404(Post, pk=self.kwargs["post_id"])
+        form.instance.post = post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("post-detail", kwargs={"pk": self.kwargs["post_id"]})
 
 # Update Comment
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
