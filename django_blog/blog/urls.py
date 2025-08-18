391317f81c115b4261f69d0b5f11f75848811059
Gacheru <johngacheru533@gmail.com>
2025-08-18 16:21:37 +0300

    updated urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,11 +17,11 @@ urlpatterns =[
     path('profile/', views.profile, name='profile'),
     path('posts/', views.posts, name='posts'),
     
-    path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post', views.PostListView.as_view(), name='post-list'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
     
 ]
