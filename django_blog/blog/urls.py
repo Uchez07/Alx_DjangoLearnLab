64b389aec285c5a81f6d37f4ed1bca5cb753bc89
Gacheru <johngacheru533@gmail.com>
2025-08-23 11:25:04 +0300

    comment urls updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,8 +23,8 @@ urlpatterns =[
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
-    path('posts/<int:pk>/comments/new/', add_comment, name='add-comment'),
-    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/comment/new/', add_comment, name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     
 ]
